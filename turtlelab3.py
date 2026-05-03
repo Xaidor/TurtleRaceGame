59c65f439df8ec5f21dd2c26ee6075b5d8f89d46
Xaidor <morekaylen@gmail.com>
2026-05-03 14:56:41 -0400

    Readme file added

--- turtlelab3.py
+++ turtlelab3.py
@@ -1,25 +1,3 @@
-# Goal
-# Using turtle, create a race between 3 racers, each a different color.
-# Ask the user to pick a racer and tell the user if their racer won or lost.
-# Incorporate special moves (power-ups) that affect the race outcome.
-# Step 1: Draw the race layout (start line and finish line).
-# Step 2: Create and position the racers.
-# Step 3: Randomize each racer's forward movement.
-# Step 4: Add a twist: each racer gets a mystery box after 3 moves.
-#         The mystery box gives either:
-#           - a speed boost, OR
-#           - a fireball attack that spins and slows the other racers.
-# Step 5: Identify the winner and display the result on the screen.
-
-# Things to still do:
-#           - MysterBox Function
-# The mysterbox would need to be called in the StartRace function. I will disappear
-# after a racer reaches it at a set loctation or random loction. The locations of 
-# each mysterbox must be inline of racers path. 
-# For testing purposes, the powerup is given after each racers 3rd move.
-#           - MysteryBox Image 
-# Determine the size, color, and style. Will it move or be startionary?
-
 # Import libraries 
 import time
 import random 
